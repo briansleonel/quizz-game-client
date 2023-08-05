7f3f84ef7ec091785a9d93ee2b74fd12e7dddad9
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-05 00:47:56 -0300

    feat: add set-value from custom hook

--- src/hooks/useFormInput.ts
+++ src/hooks/useFormInput.ts
@@ -11,10 +11,37 @@ export function useFormInput(initialValue: string) {
         setValue("");
     }
 
+    function setInput(value: string) {
+        setValue(value);
+    }
+
     const inputProps = {
         value: value,
         onChange: handleChange,
     };
 
-    return { inputProps, resetInput };
+    return { inputProps, resetInput, setInput };
+}
+
+export function useFormTextArea(initialValue: string) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleChange(e: React.FormEvent<HTMLTextAreaElement>): void {
+        setValue(e.currentTarget.value);
+    }
+
+    function resetInput(): void {
+        setValue("");
+    }
+
+    function setInput(value: string) {
+        setValue(value);
+    }
+
+    const inputProps = {
+        value: value,
+        onChange: handleChange,
+    };
+
+    return { inputProps, resetInput, setInput };
 }
