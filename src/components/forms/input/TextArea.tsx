5dd0ff0d21e53ce6ac18ebd03f2406bd7dc85525
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-05 00:46:52 -0300

    fix: change text-area input

--- src/components/forms/input/TextArea.tsx
+++ src/components/forms/input/TextArea.tsx
@@ -1,37 +1,26 @@
 import { classNames } from "@/libs/classNames";
-import { useFormContext } from "react-hook-form";
 
 interface Props {
     name: string;
-    label: string;
     placeholder?: string;
     className?: string;
+    inputProps: {
+        value: string;
+        onChange: (e: React.FormEvent<HTMLTextAreaElement>) => void;
+    };
 }
 
-export default function TextArea({
-    name,
-    className,
-    placeholder,
-    label,
-}: Props) {
-    const {
-        register,
-        formState: { errors },
-    } = useFormContext();
+export function TextArea({ inputProps, name, className, placeholder }: Props) {
     return (
-        <div className={classNames("w-full", className ? className : "")}>
-            <div className="text-start mb-1 w-full">
-                <label htmlFor={name}>{label}</label>
-            </div>
-            <textarea
-                placeholder={placeholder}
-                id={name}
-                className={`w-full resize-none px-2 py-1 transition-colors ease-in-out duration-500 outline-none rounded-md text-gray-950 border border-gray-400 bg-stone-50 focus:border-blue-600 ${
-                    errors[name] ? "border-red-500 focus:border-red-500" : ""
-                }`}
-                rows={3}
-                {...register(name)}
-            ></textarea>
-        </div>
+        <textarea
+            placeholder={placeholder}
+            id={name}
+            className={classNames(
+                "w-full px-2 py-1 transition-colors ease-in-out duration-500 outline-none rounded-md text-gray-950 border border-gray-400 bg-neutral-50 focus:border-violet-900 drop-shadow",
+                className ? className : ""
+            )}
+            rows={3}
+            {...inputProps}
+        ></textarea>
     );
 }
