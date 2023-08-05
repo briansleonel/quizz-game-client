a53ba017e989cf76580e5b5611559d6b5ede099b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-05 00:54:41 -0300

    feat: add question form page

---
+++ src/app/dashboard/question/[id]/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import QuestionForm from "@/components/forms/question/QuestionForm";
+import QuestionFormUpgrade from "@/components/forms/question/QuestionFrm";
+import questionService from "@/services/question.service";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Page({ params }: { params: { id: string } }) {
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["question", params.id],
+        queryFn: () => questionService.getQuestion(params.id),
+    });
+
+    return params.id === "new" ? (
+        <QuestionForm />
+    ) : error && error instanceof Error ? (
+        <p>Error: {error.message}</p>
+    ) : isLoading ? (
+        <p>Cargando datos...</p>
+    ) : data ? (
+        <QuestionFormUpgrade question={data.data} />
+    ) : null;
+}
