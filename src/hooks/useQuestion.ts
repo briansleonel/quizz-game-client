31cbde08ddba8680ca1cee987fd52f519eba69ce
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-09 15:11:40 -0300

    feat: add mutation for update question

--- src/hooks/useQuestion.ts
+++ src/hooks/useQuestion.ts
@@ -1,6 +1,6 @@
+import { toastError, toastSuccess } from "@/libs/toast";
 import questionService from "@/services/question.service";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "react-toastify";
 
 export function useDeleteQuestionMutation() {
     const queryClient = useQueryClient();
@@ -9,13 +9,13 @@ export function useDeleteQuestionMutation() {
         mutationFn: questionService.deleteQuestion,
         onSuccess: (data) => {
             console.log(data);
-            toast.success(data.message);
+            toastSuccess(data.message);
             // Actualizar los datos después de eliminar un usuario
             queryClient.invalidateQueries({ queryKey: ["questions"] });
         },
         onError: (err) => {
             if (err instanceof Error) {
-                toast.error(err.message);
+                toastError(err.message);
             }
         },
     });
@@ -30,16 +30,35 @@ export function useChangeVerificationQuestion() {
         mutationFn: questionService.changeVerification,
         onSuccess: (data) => {
             console.log(data);
-            toast.success(data.message);
+            toastSuccess(data.message);
             // Actualizar los datos después de eliminar un usuario
             queryClient.invalidateQueries({ queryKey: ["questions"] });
         },
         onError: (err) => {
             if (err instanceof Error) {
-                toast.error(err.message);
+                toastError(err.message);
             }
         },
     });
 
     return changeVerifiedQuestionMutation;
 }
+
+export function useUpdateQuestionMutation() {
+    const updateQuestionMutation = useMutation({
+        mutationFn: questionService.updateQuestion,
+
+        onSuccess: (data) => {
+            toastSuccess(data.message as string);
+        },
+        onError: (err) => {
+            if (err instanceof Error) {
+                toastError(err.message);
+            }
+        },
+    });
+
+    return updateQuestionMutation;
+}
+
+export function useQuestion() {}
