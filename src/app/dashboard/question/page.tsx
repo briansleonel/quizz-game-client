6f7cd9621b66ec5701b714ab00478fad50079204
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-14 14:47:01 -0300

    feat: add component

--- src/app/dashboard/question/page.tsx
+++ src/app/dashboard/question/page.tsx
@@ -1,13 +1,12 @@
 import TableQuestions from "@/components/data-table/table-question/TableQuestion";
-import Filters from "@/components/filter/Filters";
-import QuestionForm from "@/components/forms/question/QuestionForm";
+import FiltersQuestion from "@/components/filter/FiltersQuestion";
 import { Title } from "@/components/layout/Title";
 
 export default function QuestionPage() {
     return (
         <div className="w-full flex flex-col items-center gap-4 md:gap-6 py-8 px-4 md:p-8">
             <Title>Preguntas</Title>
-            <Filters />
+            <FiltersQuestion />
             <TableQuestions />
         </div>
     );
--- src/libs/role.enum.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Los distintos tipos de roles de usuario disponibles en la api
- */
-export enum Role {
-    ADMIN = "admin",
-    EDITOR = "editor",
-    PLAYER = "player",
-}
