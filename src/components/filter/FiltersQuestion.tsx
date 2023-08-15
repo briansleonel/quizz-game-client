018b25958300ac852a451159f679d253e16f1184
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-15 18:50:03 -0300

    feat: add button to create new question

--- src/components/filter/FiltersQuestion.tsx
+++ src/components/filter/FiltersQuestion.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { Plus, PlusLg, Search } from "react-bootstrap-icons";
+import { Plus, Search } from "react-bootstrap-icons";
 import Button from "../button/ButtonPrimary";
 import { SearchInput } from "../forms/input/SearchInput";
 import { useFormInput } from "@/hooks/useFormInput";
 import { useAppDispatch, useAppSelector } from "@/store/hooks.redux";
 import { changeQuestionSearchText } from "@/store/features/filters.question";
 import ModalQuestionFilters from "../modal/ModalQuestionFilters";
+import { useNavigationRouter } from "@/hooks/useNavigationRouter";
 
 export default function FiltersQuestion() {
     const dispatch = useAppDispatch();
     const filtersQuestion = useAppSelector((state) => state.questionFilters);
+
+    const router = useNavigationRouter();
 
     // Search input
     const searchInput = useFormInput(filtersQuestion.searchText);
@@ -27,6 +30,10 @@ export default function FiltersQuestion() {
         searchInput.resetInput();
     };
 
+    const addNewQuestion = () => {
+        router.goTo("/dashboard/question/new");
+    };
+
     return (
         <section className="grid grid-cols-2 md:grid-cols-7 lg:grid-cols-9 items-center w-full gap-2">
             <SearchInput
@@ -38,13 +45,17 @@ export default function FiltersQuestion() {
             />
             <Button
                 onClick={() => handlerSearch()}
-                className="bg-blue-600 hover:bg-blue-700 flex items-center justify-center gap-2 text-sm drop-shadow col-span-2 md:col-span-1"
+                className="flex items-center justify-center gap-2 text-sm drop-shadow col-span-2 md:col-span-1"
             >
                 <Search className="w-3 h-3" />
                 <span>Buscar</span>
             </Button>
             <ModalQuestionFilters />
-            <Button className="flex items-center justify-center gap-2 text-sm bg-emerald-500 hover:bg-emerald-600">
+            <Button
+                className="flex items-center justify-center gap-2 text-sm bg-emerald-500 hover:bg-emerald-600"
+                onClick={() => addNewQuestion()}
+                title="Agregar nueba pregunta"
+            >
                 <Plus className="w-5 h-5" /> <span>Nuevo</span>
             </Button>
         </section>
