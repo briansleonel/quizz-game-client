2cc7e5ffacdbf91919bc48b9c66c600bb2634d6e
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-16 17:44:18 -0300

    feat: add services to create, update and delete a question

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,11 +1,13 @@
 import { __instanceAxios, endpointsAPI } from "@/config/config";
+import { APIResponse } from "@/types/api";
 import { IQuestionCategory } from "@/types/questionCategory";
 import { isAxiosError } from "axios";
 
-export async function getAllCategories() {
+async function getAllCategories() {
     try {
-        const res = await __instanceAxios.get(endpointsAPI.CATEGORY);
-        return res.data.data as Array<IQuestionCategory>;
+        const response = await __instanceAxios.get(endpointsAPI.CATEGORY);
+        console.log(response.data);
+        return response.data.data as Array<IQuestionCategory>;
     } catch (error) {
         // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
         if (isAxiosError(error)) {
@@ -19,8 +21,100 @@ export async function getAllCategories() {
     }
 }
 
+async function getAllCategoriesPagination() {
+    try {
+        const response = await __instanceAxios.get(endpointsAPI.CATEGORY);
+        console.log(response.data);
+        return response.data as APIResponse<Array<IQuestionCategory>>;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            }
+        }
+        if (error instanceof Error) throw new Error(error.message);
+    }
+}
+
+async function deleteCategory(id: string) {
+    try {
+        const response = await __instanceAxios.delete(
+            `${endpointsAPI.CATEGORY}/${id}`
+        );
+        return response.data;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            } else {
+                throw new Error(error.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error sin procesar");
+        }
+    }
+}
+
+async function updateCategory(category: IQuestionCategory) {
+    try {
+        const response = await __instanceAxios.put(
+            `${endpointsAPI.CATEGORY}/${category._id}`,
+            category
+        );
+        return response.data;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            } else {
+                throw new Error(error.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error sin procesar");
+        }
+    }
+}
+
+async function addCategory(category: IQuestionCategory) {
+    try {
+        const response = await __instanceAxios.post(endpointsAPI.CATEGORY, {
+            name: category.name,
+        });
+        return response.data;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            } else {
+                throw new Error(error.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error sin procesar");
+        }
+    }
+}
+
 const categoryService = {
     getAllCategories,
+    deleteCategory,
+    updateCategory,
+    addCategory,
+    getAllCategoriesPagination,
 };
 
 export default categoryService;
