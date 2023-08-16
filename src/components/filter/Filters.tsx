2efbb316123cf4075a76a20ff5b3e9a49f14d3d9
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-16 11:35:07 -0300

    refractor: change to list-box of verified

--- src/components/filter/Filters.tsx
+++ src/components/filter/Filters.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Filter, Search } from "react-bootstrap-icons";
+import { Search } from "react-bootstrap-icons";
 import Button from "../button/ButtonPrimary";
 import { SearchInput } from "../forms/input/SearchInput";
 import {
@@ -11,27 +11,22 @@ import {
 import { IData } from "@/types/util";
 import { useAppDispatch, useAppSelector } from "@/store/hooks.redux";
 import { useFormInput } from "@/hooks/useFormInput";
-import { ChangeEvent, useState } from "react";
-
-const optionsVerified: Array<IData> = [
-    { label: "Todos", value: Verified.ALL },
-    { label: "Verificados", value: Verified.VERIFIED },
-    { label: "No verificados", value: Verified.NOVERIFIED },
-];
+import { useEffect, useState } from "react";
+import ListBoxData from "../forms/list-box/ListBoxData";
+import { optionsVerified } from "@/libs/enums/filter.enum";
 
 export default function Filters() {
     const dispatch = useAppDispatch();
-    const filters = useAppSelector((state) => state.fitlers);
+
     const searchInput = useFormInput("");
 
-    const [expanded, setExpanded] = useState(false);
-
-    const [showMobile, setShowMobile] = useState(false);
+    const [selectedVerified, setSelectedVerified] = useState<IData>(
+        optionsVerified[0]
+    );
 
     const searchHandle = () => {
         dispatch(
             changeSearchText({
-                ...filters,
                 searchText: searchInput.inputProps.value,
             })
         );
@@ -41,74 +36,50 @@ export default function Filters() {
         searchInput.resetInput();
         dispatch(
             changeSearchText({
-                ...filters,
                 searchText: "",
             })
         );
     };
 
-    const changeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    // Effect to Verified
+    useEffect(() => {
         dispatch(
             changeFilterVerified({
-                ...filters,
-                verified: e.currentTarget.value as Verified,
+                verified: selectedVerified.value as Verified,
             })
         );
-    };
+    }, [selectedVerified, dispatch]);
 
     return (
-        <section className="w-full">
-            <button
-                className="w-full p-2 rounded select-none bg-zinc-900 text-white flex justify-between items-center font-light text-xs uppercase md:hidden"
-                onClick={() => setShowMobile(!showMobile)}
+        <section className="grid grid-cols-2 md:grid-cols-7 lg:grid-cols-9 items-center w-full gap-2">
+            <SearchInput
+                type="text"
+                name="searchInput"
+                placeholder="Nombre completo o nombre de usuario..."
+                inputProps={searchInput.inputProps}
+                className="col-span-2 md:col-span-5 lg:col-span-5"
+                onClick={() => resetSearchInput()}
+            />
+            <Button
+                onClick={() => searchHandle()}
+                className="bg-blue-600 hover:bg-blue-700 flex items-center justify-center col-span-2 md:col-span-2 lg:col-span-1 py-2 text-sm shadow"
             >
-                <span>Filtros</span> <Filter className="" />
-            </button>
-            <div
-                className={`w-full bg-neutral-100 md:bg-transparent rounded overflow-hidden transition-[max-height] duration-500 ease-in drop-shadow md:max-h-fit ${
-                    showMobile ? "max-h-80" : "max-h-0"
-                }`}
-            >
-                <div className="grid p-4 md:p-0 grid-cols-1 md:grid-cols-8 gap-2 md:gap-4 lg:gap-2 items-end md:items-center">
-                    <SearchInput
-                        type="text"
-                        name="searchInput"
-                        placeholder="Nombre completo o nombre de usuario..."
-                        inputProps={searchInput.inputProps}
-                        className="col-span-1 md:col-span-6 lg:col-span-5"
-                        onClick={() => resetSearchInput()}
-                    />
-                    <Button
-                        onClick={() => searchHandle()}
-                        className="bg-blue-600 hover:bg-blue-700 flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1 py-2 text-sm shadow"
-                    >
-                        <span>Buscar</span>
-                        <Search className="ml-3" />
-                    </Button>
+                <span>Buscar</span>
+                <Search className="ml-3" />
+            </Button>
 
-                    <label
-                        htmlFor="verified"
-                        className="w-auto md:w-full whitespace-pre text-base col-span-1 md:text-end -mb-2 md:mb-0"
-                    >
-                        Mostrar:
-                    </label>
-                    <select
-                        name="verified"
-                        id="verified"
-                        onChange={(e) => changeSelect(e)}
-                        className="w-full px-2 md:px-3 py-1.5 border rounded text-gray-950 border-gray-400 bg-zinc-50 focus:outline-none text-sm uppercase drop-shadow col-span-1 md:col-span-2 lg:col-span-1"
-                    >
-                        {optionsVerified.map((e) => (
-                            <option
-                                key={e.label}
-                                value={e.value}
-                                className="py-2 hover:bg-neutral-400 checked:!bg-neutral-400"
-                            >
-                                {e.label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+            <div className="flex items-center flex-col gap-0 col-span-2 md:flex-row md:gap-2 md:col-span-7 lg:col-span-3">
+                <label
+                    htmlFor="verified"
+                    className="w-full whitespace-pre text-base text-start md:text-end md:mb-0"
+                >
+                    Mostrar
+                </label>
+                <ListBoxData
+                    selected={selectedVerified}
+                    setSelected={setSelectedVerified}
+                    options={optionsVerified}
+                />
             </div>
         </section>
     );
