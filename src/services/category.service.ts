e901142093e7bc73009a58a26a92d5dbd0318158
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-16 23:44:25 -0300

    docs: add documentation to service

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,12 +1,18 @@
 import { __instanceAxios, endpointsAPI } from "@/config/config";
-import { APIResponse } from "@/types/api";
+import { APIResponse, PaginationFetch } from "@/types/api";
 import { IQuestionCategory } from "@/types/questionCategory";
 import { isAxiosError } from "axios";
 
+/**
+ * Permite envíar una petición GET a "/api/category/all" para devolver todas las categorías almacenadas en la API, sin paginación de datos
+ *
+ * @returns todas las categorias
+ */
 async function getAllCategories() {
     try {
-        const response = await __instanceAxios.get(endpointsAPI.CATEGORY);
-        console.log(response.data);
+        const response = await __instanceAxios.get(
+            `${endpointsAPI.CATEGORY}/all`
+        );
         return response.data.data as Array<IQuestionCategory>;
     } catch (error) {
         // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
@@ -21,10 +27,21 @@ async function getAllCategories() {
     }
 }
 
-async function getAllCategoriesPagination() {
+/**
+ * Permite envíar una petición GET a "/api/category" para devolver todas las categorías de la API, a través de la paginación de los datos.
+ * Debe enviarse:
+ *      * limit: cantidad de datos a mostrar por página
+ *      * page: página de datos a mostrar
+ *
+ * @param PaginationFetch datos de paginación "limit" - "page"
+ * @returns
+ */
+async function getAllCategoriesPagination({ limit, page }: PaginationFetch) {
+    const paginationData = `?page=${page}&limit=${limit}`;
     try {
-        const response = await __instanceAxios.get(endpointsAPI.CATEGORY);
-        console.log(response.data);
+        const response = await __instanceAxios.get(
+            endpointsAPI.CATEGORY + paginationData
+        );
         return response.data as APIResponse<Array<IQuestionCategory>>;
     } catch (error) {
         // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
@@ -39,6 +56,12 @@ async function getAllCategoriesPagination() {
     }
 }
 
+/**
+ * Permite envíar una petición DELETE a "/api/category/:id" para eliminar una determinada categoría, a partir de un "id" recibido como parámetro
+ *
+ * @param id identificador de categoría
+ * @returns response de la API
+ */
 async function deleteCategory(id: string) {
     try {
         const response = await __instanceAxios.delete(
@@ -62,6 +85,11 @@ async function deleteCategory(id: string) {
     }
 }
 
+/**
+ * Permite envíar una petición PUT a "/api/category/:id" para actualizar una determinada categoría. Este método debe recibir como parámetro una catergoría con un ID válido para poder realizar los cambios
+ * @param category categoría a actualizar
+ * @returns response de la API
+ */
 async function updateCategory(category: IQuestionCategory) {
     try {
         const response = await __instanceAxios.put(
@@ -86,6 +114,11 @@ async function updateCategory(category: IQuestionCategory) {
     }
 }
 
+/**
+ * Permite envíar una petición POST a "/api/category" para poder agregar una nueva categoría a la BD. Debe recibir como parámetro los datos de la categoría a agregar.
+ * @param category categoría nueva
+ * @returns response de la API
+ */
 async function addCategory(category: IQuestionCategory) {
     try {
         const response = await __instanceAxios.post(endpointsAPI.CATEGORY, {
