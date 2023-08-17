02b06fe41260a10161f1e945b450e21d28cd4936
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-17 20:02:15 -0300

    feat: add custom hook to logout

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,47 @@
+import { toastError, toastSuccess } from "@/libs/sonner/sonner.toast";
+import { deleteAuthLocalStorage } from "@/libs/state.localStorage";
+import userService from "@/services/user.service";
+import { logout } from "@/store/features/authSlice";
+import { useAppDispatch } from "@/store/hooks.redux";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+/**
+ * Hook personalizado que maneja el logout de un usuario en la aplicación.
+ * Si todo salió bien, elimina los datos almacenados en local de un usuario.
+ * En caso contrario se muestra una notificación del error sucedido.
+ * @returns hook para realizar el loggeo de un usuario
+ */
+export function useLogoutMutation() {
+    const dispatch = useAppDispatch();
+    const router = useRouter();
+
+    const logoutMutation = useMutation({
+        mutationFn: userService.logout,
+        onSuccess: (data, variables, context) => {
+            try {
+                dispatch(logout()); // elimino los datos de la store
+                deleteAuthLocalStorage(); // elimino los datos de localstorage
+                toastSuccess(data.message);
+                router.push("/"); // redirecciono a la página /dashboard
+            } catch (error) {
+                if (error instanceof Error) toastError(error.message);
+            }
+        },
+        onError: (err, variables, context) => {
+            if (err instanceof Error) toastError(err.message);
+        },
+    });
+
+    return logoutMutation;
+}
+
+export function useLogout() {
+    const logoutMutation = useLogoutMutation();
+
+    async function handlerLogout() {
+        await logoutMutation.mutateAsync();
+    }
+
+    return { handlerLogout };
+}
