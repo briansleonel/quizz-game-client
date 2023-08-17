bd3e11463970d8c789bb47205e6d9094b0fd0ea2
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-17 20:00:14 -0300

    feat: add logout service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -15,9 +15,29 @@ export interface QueryFetch extends PaginationFetch {
  * @param user credenciales de un usuario
  * @returns respuesta del servidor
  */
-export async function login(user: ILogin) {
+async function login(user: ILogin) {
     try {
         const response = await __instanceAxios.post(endpointsAPI.LOGIN, user);
+
+        return response.data;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error al verificar las credenciales");
+        }
+    }
+}
+
+async function logout() {
+    try {
+        const response = await __instanceAxios.post(endpointsAPI.LOGOUT);
 
         return response.data;
     } catch (error) {
@@ -150,6 +170,7 @@ const userService = {
     getUsers,
     deleteUser,
     changeVerification,
+    logout,
 };
 
 export default userService;
