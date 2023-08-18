5463f232bff748002cee89ad17a1c77e25247a19
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-18 13:15:55 -0300

    fix: fix hydratate error

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,18 +1,30 @@
+"use client";
+
 import TableQuestionCategory from "@/components/data-table/table-category/TableQuestionCategory";
-import CategoryForm from "@/components/forms/category/CategoryForm";
 import ContainerUtil from "@/components/layout/ContainerUtil";
 import { Title } from "@/components/layout/Title";
 import ModalCategory from "@/components/modal/ModalCategory";
+import { Role } from "@/libs/enums/role.enum";
+import { useAppSelector } from "@/store/hooks.redux";
+import { useEffect, useState } from "react";
 
 export default function CategoryPage() {
+    const { user } = useAppSelector((state) => state.auth);
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     return (
         <ContainerUtil>
             <Title>Categorías</Title>
             {/*<CategoryForm />*/}
-            <div className="w-full flex justify-end">
-
-            <ModalCategory edit={false} />
-            </div>
+            {isClient && user.role === Role.ADMIN && (
+                <div className="w-full flex justify-end">
+                    <ModalCategory edit={false} />
+                </div>
+            )}
             <TableQuestionCategory />
         </ContainerUtil>
     );
