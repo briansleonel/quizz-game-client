6a20866a7df6ecf34ea3c65199838275d72a1ed2
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-19 13:25:21 -0300

    refractor: uppdate middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import {
+    loadStateAuthLocalStorage,
+    saveStateAuthLocalStorage,
+} from "./libs/state.localStorage";
+
+interface DecodeJwt {
+    id: string;
+    role: string;
+    iat: number;
+    exp: number;
+}
 
 export async function middleware(request: NextRequest) {
-    const jwt = request.cookies.get("token");
+    const token = request.cookies.get("token");
 
     try {
-        if (!jwt) return NextResponse.redirect(new URL("/login", request.url));
+        if (!token)
+            return NextResponse.redirect(new URL("/login", request.url));
+
+        //console.log(token);
+
+        //const decodeJwt = jwt.decode(token.value) as DecodeJwt;
+
+        //console.log(decodeJwt);
+
+        //saveStateAuthLocalStorage({ _id: decodeJwt.id, role: decodeJwt.role });
+
+        //const auth = loadStateAuthLocalStorage();
 
         return NextResponse.next();
     } catch (error) {
