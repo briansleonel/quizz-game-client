fb3fa4e9dba73a8143e1e39be119fb8e801c7b3e
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 15:54:22 -0300

    refractor: change styles of component

--- src/components/trivia/ShowQuestion.tsx
+++ src/components/trivia/ShowQuestion.tsx
@@ -1,7 +1,9 @@
 export default function ShowQuestion({ question }: { question: string }) {
     return (
-        <div className="w-full bg-white text-black font-normal md:font-medium text-2xl text-center p-6  rounded">
-            {question}
+        <div className="max-w-4xl mx-auto rounded backdrop-blur-sm backdrop-filter text-white bg-zinc-950/40 p-4 drop-shadow-2xl shadow-lg">
+            <div className=" bg-white text-neutral-900 font-normal md:font-semibold text-4xl text-center leading-snug p-4 px-6 rounded">
+                <h4>{question}</h4>
+            </div>
         </div>
     );
 }
