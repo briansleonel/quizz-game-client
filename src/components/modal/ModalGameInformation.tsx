bf7aff0792266e0db05646e6c0c87ee440486ad4
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 16:00:12 -0300

    styles: change styles

--- src/components/modal/ModalGameInformation.tsx
+++ src/components/modal/ModalGameInformation.tsx
@@ -2,20 +2,26 @@ import { CheckCircle, Eye, XCircle } from "react-bootstrap-icons";
 import ModalDialog from "./ModalDialog";
 import Button from "../button/ButtonPrimary";
 import { useState } from "react";
+import ButtonTrivia from "../trivia/ButtonTrivia";
+import { useAppSelector } from "@/store/hooks.redux";
 
 interface Props {
     closeModal: () => void;
     isOpen: boolean;
     question: { isCorrect: boolean; correct: string; description: string };
+    nextQuestion: () => void;
 }
 
 export default function ModalGameInformation({
     closeModal,
     isOpen,
     question,
+    nextQuestion,
 }: Props) {
     // Estado para controlar si se muestra o no la descripción disponible en la pregunta
     const [showDescription, setShowDescription] = useState(false);
+
+    const { hasNext } = useAppSelector((state) => state.game);
 
     return (
         <>
@@ -25,7 +31,7 @@ export default function ModalGameInformation({
                 title=""
                 showBtnClose={false}
             >
-                <div className="flex flex-col w-full items-center justify-center gap-6">
+                <section className="flex flex-col w-full items-center justify-center gap-6">
                     {/** ESTADO DE LA OPCION SELECCIONADA */}
                     <h3 className="uppercase text-2xl md:text-3xl font-light text-center">
                         ¡Respuesta{" "}
@@ -42,9 +48,7 @@ export default function ModalGameInformation({
                     </span>
 
                     {/** MOSTRAR RESPUESTA CORRECTA Y DESCRIPCIÓN */}
-                    <div
-                        className={`w-full border border-indigo-500 text-indigo-600 rounded-md p-2 text-sm`}
-                    >
+                    <div className="w-full border border-indigo-500 text-indigo-600 rounded-md p-2 text-sm mt-4">
                         <div className="w-full flex justify-between items-center">
                             <p className="font-semibold uppercase">
                                 <span className="font-normal">
@@ -68,7 +72,26 @@ export default function ModalGameInformation({
                             <p className="mt-2">{question.description}</p>
                         )}
                     </div>
-                </div>
+
+                    {/** BOTONES PARA CONTINUAR O FINALIZAR LA TRIVIA */}
+                    {hasNext ? (
+                        <ButtonTrivia
+                            onClickFn={() => nextQuestion()}
+                            title="Siguiente pregunta"
+                            className="mt-8 !bg-purple-600 text-white hover:text-white border-none"
+                        >
+                            Continuar
+                        </ButtonTrivia>
+                    ) : (
+                        <ButtonTrivia
+                            onClickFn={() => function () {}}
+                            title=""
+                            className="mt-8 !bg-neutral-900 text-white hover:text-white border-none"
+                        >
+                            Finalizar
+                        </ButtonTrivia>
+                    )}
+                </section>
             </ModalDialog>
         </>
     );
