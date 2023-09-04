147be77180603f165a9b4dd368ad646390bf26a8
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 21:01:40 -0300

    styles: change styles

--- src/components/trivia/ReplyButton.tsx
+++ src/components/trivia/ReplyButton.tsx
@@ -13,7 +13,7 @@ export default function ReplyButton({
         <button
             type="button"
             onClick={() => onSelectAnswer(option)}
-            className={`bg-white text-black w-full p-4 font-medium uppercase text-base md:text-2xl  rounded hover:bg-indigo-700 active:bg-indigo-700 active:text-white hover:text-white transition-all duration-500 hover:scale-105 shadow shadow-neutral-900 ${
+            className={`bg-white text-black w-full p-4 font-medium uppercase text-base md:text-2xl  rounded hover:bg-indigo-700 active:bg-indigo-700 active:text-white hover:text-white transition-all duration-500 shadow shadow-neutral-900 ${
                 className ?? ""
             }`}
         >
