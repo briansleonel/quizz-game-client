42de2b977335883aa70664089ac85bf66f5caa6b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 22:25:38 -0300

    styles: change styles

--- src/components/trivia/ReplyButton.tsx
+++ src/components/trivia/ReplyButton.tsx
@@ -2,22 +2,22 @@ interface Props {
     className?: string;
     option: string;
     onSelectAnswer: (selected: string) => void;
+    children: React.ReactNode;
 }
 
 export default function ReplyButton({
     option,
     onSelectAnswer,
     className,
+    children,
 }: Props) {
     return (
         <button
             type="button"
             onClick={() => onSelectAnswer(option)}
-            className={`bg-white text-black w-full p-4 font-medium uppercase text-base md:text-2xl  rounded hover:bg-indigo-700 active:bg-indigo-700 active:text-white hover:text-white transition-all duration-500 shadow shadow-neutral-900 ${
-                className ?? ""
-            }`}
+            className={`relative bg-white text-black w-full p-4 font-medium uppercase text-base md:text-2xl  rounded hover:bg-indigo-700 active:bg-indigo-700 active:text-white hover:text-white transition-all duration-500 shadow shadow-neutral-900 ${className}`}
         >
-            {option}
+            {children}
         </button>
     );
 }
