ca3b50a1bc3aae6ac445a4adbf130b189af78861
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 21:02:12 -0300

    feat: add component to show result

---
+++ src/components/trivia/AnswerQuestion.tsx
@@ -0,0 +1,47 @@
+import { useAppSelector } from "@/store/hooks.redux";
+import ButtonTrivia from "./ButtonTrivia";
+
+interface Props {
+    question: { isCorrect: boolean; correct: string; description: string };
+    nextQuestion: () => void;
+}
+
+export default function AnswerQuestion({ nextQuestion, question }: Props) {
+    const { hasNext } = useAppSelector((state) => state.game);
+
+    return (
+        <div className="w-full flex flex-col gap-8 items-center">
+            <div
+                className={`w-fit rounded py-4 px-20 transform -skew-x-12 ${
+                    question.isCorrect ? "bg-green-600" : "bg-red-600"
+                }`}
+            >
+                <div className="transform skew-x-12">
+                    <h3 className="uppercase text-2xl md:text-4xl font-light text-center text-white">
+                        ¡Respuesta{" "}
+                        {question.isCorrect ? "correcta" : "incorrecta"}!
+                    </h3>
+                </div>
+            </div>
+
+            {/** BOTONES PARA CONTINUAR O FINALIZAR LA TRIVIA */}
+            {hasNext ? (
+                <ButtonTrivia
+                    onClickFn={() => nextQuestion()}
+                    title="Siguiente pregunta"
+                    className="!bg-purple-600 text-white hover:text-white border-none shadow shadow-neutral-900"
+                >
+                    Continuar
+                </ButtonTrivia>
+            ) : (
+                <ButtonTrivia
+                    onClickFn={() => function () {}}
+                    title=""
+                    className="!bg-neutral-900 text-white hover:text-white border-none shadow shadow-neutral-900"
+                >
+                    Finalizar
+                </ButtonTrivia>
+            )}
+        </div>
+    );
+}
