2409e2c51b86be414cd6982332ad860cb33b3c4b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-03 22:27:26 -0300

    refractor: change how it shows the correct and incorrect option

--- src/components/trivia/ShowRandomOptions.tsx
+++ src/components/trivia/ShowRandomOptions.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from "@/store/hooks.redux";
 import ReplyButton from "./ReplyButton";
 import { useEffect, useState } from "react";
+import { Check, X } from "react-bootstrap-icons";
 
 export default function ShowRandomOptions({
     selectedAnswer,
@@ -38,6 +39,7 @@ export default function ShowRandomOptions({
 
     /**
      * Permite verificar si la opción que se seleccionó es la misma que la opción que se envía por parámetro. En caso de que lo sea, envía "true", y envía "false" en caso contrario.
+     *
      * @param option opcion
      * @returns true - false
      */
@@ -48,25 +50,63 @@ export default function ShowRandomOptions({
         );
     };
 
+    /**
+     * Permite verificar si una opción ingresada por parámetro es la misma que la opción correcta de la pregunta actual.
+     *
+     * @param option opcion
+     * @returns true - false
+     */
+    const isCorrect = (option: string) => {
+        if (currentQuestion) {
+            const correct =
+                currentQuestion.options[currentQuestion.correct].toLowerCase();
+            if (correct === option.toLowerCase()) return true;
+        }
+        return false;
+    };
+
     return (
         <>
-            <section className="w-full grid grid-cols-2 gap-6">
+            <section className="w-full grid grid-cols-2 gap-4">
                 {randomOptions.length !== 0 &&
                     randomOptions.map((e, i) => (
                         <ReplyButton
                             key={i}
                             option={e}
                             onSelectAnswer={onSelectAnswer}
-                            className={
+                            className={`${
                                 isCurrentCheckOption(e)
-                                    ? isCorrectOption()
-                                        ? "!bg-green-600 hover:bg-green-600 text-white font-medium"
-                                        : "!bg-red-600 hover:bg-red-600 text-white font-medium"
+                                    ? "!bg-neutral-800 text-white"
                                     : ""
-                            }
-                        />
+                            } ${
+                                !isCurrentCheckOption(e) && selectedAnswer
+                                    ? "hover:!bg-white hover:!text-black"
+                                    : ""
+                            }`}
+                        >
+                            {e}{" "}
+                            {selectedAnswer && (
+                                <span className="absolute right-4 top-1/2  transform -translate-y-1/2 font-bold">
+                                    {isCorrect(e) ? (
+                                        <Check className="w-10 h-fit text-green-500" />
+                                    ) : (
+                                        <X className="w-10 h-fit text-red-500" />
+                                    )}
+                                </span>
+                            )}
+                        </ReplyButton>
                     ))}
             </section>
         </>
     );
 }
+
+/*
+className={
+    isCurrentCheckOption(e)
+        ? isCorrectOption()
+            ? "!bg-green-600 hover:bg-green-600 text-white font-medium"
+            : "!bg-red-600 hover:bg-red-600 text-white font-medium"
+        : ""
+}
+*/
