c51e2f006a58ae42156faf6d345e2b74648e8f70
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-08 17:26:39 -0300

    styles: change styles

--- src/components/forms/game/GameForm.tsx
+++ src/components/forms/game/GameForm.tsx
@@ -22,31 +22,40 @@ export default function GameForm() {
     const router = useRouter();
 
     // Input Limit Questions
-    const inputLimit = useFormInput("1");
+    const inputLimit = useFormInput("");
 
     // Select category
     const [selectedCategory, setSelectedCategory] = useState<IQuestionCategory>(
         { _id: "", name: "" }
     );
 
+    function isNumber(value: string) {
+        const regExp = /^[0-9]+$/;
+        return regExp.test(value);
+    }
+
     const onClickFindQuestions = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        const limit: number = Number(inputLimit.inputProps.value);
+        if (isNumber(inputLimit.inputProps.value)) {
+            const limit: number = Number(inputLimit.inputProps.value);
 
-        if (limit >= 1 && limit <= 30) {
-            if (selectedCategory._id !== "") {
-                dispatch(
-                    gameSetConfig({
-                        category: selectedCategory,
-                        limit,
-                    })
-                );
-                router.push("/game/start");
+            if (limit >= 1 && limit <= 30) {
+                if (selectedCategory._id !== "") {
+                    dispatch(
+                        gameSetConfig({
+                            category: selectedCategory,
+                            limit,
+                        })
+                    );
+                    router.push("/game/start");
+                } else {
+                    toastInformation("Debe seleccionar una categoría");
+                }
             } else {
-                toastInformation("Debe seleccionar una categoría");
+                toastInformation("Debe ingresar una cantidad entre 1 y 30");
             }
         } else {
-            toastInformation("Debe ingresar una cantidad entre 1 y 30");
+            toastInformation("Debe ingresar un número");
         }
     };
 
@@ -62,22 +71,28 @@ export default function GameForm() {
             <form
                 action=""
                 method="get"
-                className="h-full flex flex-col justify-between gap-10"
+                className="h-full flex flex-col justify-between rounded gap-6 bg-white text-neutral-900 p-4"
             >
                 <div className="flex flex-col gap-4">
-                    <Label label="Cantidad de preguntas" name="limit">
-                        <InputText
-                            type="number"
-                            name="limit"
-                            inputProps={inputLimit.inputProps}
-                            className="text-neutral-900"
-                        />
-                    </Label>
-                    <Label label="Categoría" name="category">
+                    {/*<Label label="Cantidad de preguntas" name="limit">*/}
+                    <InputText
+                        type="text"
+                        name="limit"
+                        inputProps={inputLimit.inputProps}
+                        className="text-neutral-900 rounded-sm text-xl font-semibold text-center drop-shadow-none placeholder:text-gray-400 placeholder:font-normal placeholder:text-lg"
+                        placeholder="Cantidad de preguntas"
+                    />
+                    {/*</Label>*/}
+                    <Label
+                        label="Categoría"
+                        name="category"
+                        className="text-neutral-600 uppercase text-sm"
+                    >
                         <ListBoxCategory
                             selected={selectedCategory}
                             setSelected={setSelectedCategory}
                             newOption={allCategories}
+                            className="rounded-sm !text-base font-medium shadow-none py-1"
                         />
                     </Label>
                 </div>
@@ -85,7 +100,7 @@ export default function GameForm() {
                 <ButtonTrivia
                     onClickFn={onClickFindQuestions}
                     title="Iniciar trivia"
-                    className="mx-auto"
+                    className="w-full !bg-neutral-900 text-white hover:!bg-neutral-950 font-medium !py-3"
                 >
                     Iniciar
                 </ButtonTrivia>
